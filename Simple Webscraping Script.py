394b629e2545dafa1d5b3ce40d3250f9ae2009a5
Samarth-Bahuguna <98493097+Samarth-Bahuguna@users.noreply.github.com>
2022-02-03 01:19:11 +0530

    Update Simple Webscraping Script.py

--- Simple Webscraping Script.py
+++ Simple Webscraping Script.py
@@ -15,7 +15,6 @@ soup = BeautifulSoup(html_data,"html5lib")
 
 laptop_product_block = soup.find_all("div", class_="_1AtVbE col-12-12")
 
-
 #Creation of a dataframe
 laptop_data=pandas.DataFrame(columns = ["Laptop Name", "Price","Rating"])
 
@@ -27,14 +26,13 @@ for laptop in laptop_product_block:
     laptop_name = laptop.find("div", class_="_4rR01T").text
     laptop_price = laptop.find("div", class_="_30jeq3 _1_WHN1").text
     laptop_rating = laptop.find("div", class_="_3LWZlK").text
-
-    
-    
     
     laptop_data= laptop_data.append({"Laptop Name": laptop_name, "Price":laptop_price, "Rating":laptop_rating}, ignore_index= True)
 
 print(laptop_data)   #Printing the dataframe
-laptop_data.to_csv("File1.csv", encoding="utf-8") #encoding="utf-8" ----> It is there to prevent encoding error
+laptop_data.to_csv("File1.csv", encoding="utf-8")
+#encoding="utf-8" ----> It is there to prevent encoding error
+#File 1 is saved in the current directory
 
 
 
